34dc3b2994833945a27eace923aa8a1c6900f193
mondrake <mondrake@mondrake.org>
2015-06-01 16:50:46 +0200

    - fixed 'cache.textimage' service definition after upgrade to Symfony 2.7.0 (see #2470693) - cleaned up JS

--- js/textimage.color_element.js
+++ js/textimage.color_element.js
@@ -8,17 +8,18 @@
 
 (function ($) {
 
-Drupal.behaviors.textimageColorElement = {
+  "use strict";
 
-  attach: function(context, settings) {
-    $('.textimage-color-element .form-color', context).once('textimage-color-element').each(function(index) {
-      $(this).on('change', function(event) {
-        var suffix = $(this).parents('.textimage-color-element').find('.field-suffix').get(0);
-        $(suffix).text(this.value.toUpperCase());
+  Drupal.behaviors.textimageColorElement = {
+
+    attach: function (context, settings) {
+      $('.textimage-color-element .form-color', context).once('textimage-color-element').each(function (index) {
+        $(this).on('change', function (event) {
+          var suffix = $(this).parents('.textimage-color-element').find('.field-suffix').get(0);
+          $(suffix).text(this.value.toUpperCase());
+        });
       });
-    });
-  }
+    }
 
-}
-
+  };
 })(jQuery);
